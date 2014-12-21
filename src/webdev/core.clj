00338c8a86d71c99803fda08039162595e99fe17
Bob Nadler <bnadlerjr@gmail.com>
2014-12-21 12:33:49 -0500

    Add model.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,8 +1,11 @@
 (ns webdev.core
+  (:require [webdev.model :as model])
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]))
+
+(def db "jdbc:postgresql://localhost/webdev")
 
 (defn greet [req]
   {:status 200 :body "Hello, world! Now with reloading!" :headers {}})
@@ -16,7 +19,9 @@
   (not-found "Page not found."))
 
 (defn -main [port]
+  (model/create-table db)
   (jetty/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main [port]
+  (model/create-table db)
   (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
