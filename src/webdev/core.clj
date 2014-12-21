bf25c6fc731424a1bd4d3fb5889fdf3c48840add
Bob Nadler <bnadlerjr@gmail.com>
2014-12-21 12:47:17 -0500

    Middleware for DB and server name.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [webdev.model :as model])
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
             [compojure.route :refer [not-found]]))
 
 (def db "jdbc:postgresql://localhost/webdev")
@@ -13,10 +14,21 @@
 (defn goodbye [req]
   {:status 200 :body "Goodbye, cruel world!" :headers {}})
 
-(defroutes app
+(defroutes routes
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
   (not-found "Page not found."))
+
+(defn wrap-db [hdlr]
+  (fn [req]
+    (hdlr (assoc req :webdev/db db))))
+
+(defn wrap-server [hdlr]
+  (fn [req]
+    (assoc-in (hdlr req) [:headers "Server"] "Webdev Spike")))
+
+(def app
+  (wrap-server (wrap-db (wrap-params routes))))
 
 (defn -main [port]
   (model/create-table db)
