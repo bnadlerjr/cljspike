b8cd31da87c558e53661672ec3a8312da1020e61
Bob Nadler <bnadlerjr@gmail.com>
2014-12-22 20:16:45 -0500

    Refactor main layout.
    * create helper functions to handle CDN fallbacks
    * ensure asset library versions are the same

--- src/webdev/common/views/layout.clj
+++ src/webdev/common/views/layout.clj
@@ -2,38 +2,71 @@
   (:require [hiccup.page :refer [html5 include-css include-js]]
             [hiccup.element :refer [javascript-tag]]))
 
+(def jquery-version "2.1.3")
+(def bootstrap-version "3.3.1")
+
+(defn- bootstrap-css-cdn [version]
+  (include-css
+    (str
+      "//netdna.bootstrapcdn.com/bootstrap/"
+      version
+      "/css/bootstrap.min.css")))
+
+(defn- bootstrap-css-local-fallback [version]
+  (javascript-tag
+    (str
+      "$(document).ready(function() {"
+      "var bodyColor = $('body').css('color');"
+      "if(bodyColor != 'rgb(51, 51, 51)') {"
+      "$(\"head\").prepend('<link rel=\"stylesheet\" href=\"/bootstrap/"
+      version
+      "/css/bootstrap.min.css\">');}});")))
+
+(defn- bootstrap-js-cdn [version]
+  (include-js
+    (str
+      "//netdna.bootstrapcdn.com/bootstrap/"
+      version
+      "/js/bootstrap.min.js")))
+
+(defn- bootstrap-js-local-fallback [version]
+  (javascript-tag
+    (str
+      "if(typeof($.fn.modal) === 'undefined') "
+      "{document.write('<script src=\"/bootstrap/"
+      version
+      "/js/bootstrap.min.js\"><\\/script>')}")))
+
+(defn- jquery-cdn [version]
+  (include-js
+    (str
+      "http://ajax.googleapis.com/ajax/libs/jquery/"
+      version
+      "/jquery.min.js")))
+
+(defn- jquery-local-fallback [version]
+  (javascript-tag
+    (str
+      "window.jQuery || document.write('<script src=\"/jquery-"
+      version
+      ".min.js\"><\\/script>')")))
+
 (defn common
   "The main layout. Uses the CDN versions of jQuery and Bootstrap. If those are
   not available, fall back to local copies."
   [& body]
-  (html5 {:lang :en}
-         [:head
-          [:title "Clj Spike"]
-          [:meta {:name :viewport
-                  :content "width=device-width, initial-scale=1.0"}]
+  (html5
+    {:lang :en}
+    [:head
+     [:title "Clj Spike"]
+     [:meta {:name :viewport
+             :content "width=device-width, initial-scale=1.0"}]
+     (bootstrap-css-cdn bootstrap-version)]
 
-          ;; Bootstrap CSS CDN
-          (include-css "//netdna.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")]
-
-         [:body
-          body
-
-          ;; jQuery CDN
-          (include-js "http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
-
-          ;; jQuery local fallback
-          (javascript-tag "window.jQuery || document.write('<script src=\"/jquery-2.1.3.min.js\"><\\/script>')")
-
-          ;; Bootstrap JS CDN
-          (include-js "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js")
-
-          ;; Bootstrap JS local fallback
-          (javascript-tag "if(typeof($.fn.modal) === 'undefined') {document.write('<script src=\"/bootstrap/js/bootstrap.min.js\"><\\/script>')}")
-
-          ;; Bootstrap CSS local fallback
-          (javascript-tag
-            (str
-              "$(document).ready(function() {"
-              "var bodyColor = $('body').css('color');"
-              "if(bodyColor != 'rgb(51, 51, 51)') {"
-              "$(\"head\").prepend('<link rel=\"stylesheet\" href=\"/bootstrap/css/bootstrap.min.css\">');}});"))]))
+    [:body
+     body
+     (jquery-cdn jquery-version)
+     (jquery-local-fallback jquery-version)
+     (bootstrap-js-cdn bootstrap-version)
+     (bootstrap-js-local-fallback bootstrap-version)
+     (bootstrap-css-local-fallback bootstrap-version)]))
