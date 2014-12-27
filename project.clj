bd31a1422ffe7ddb3a6e5b70a94a9a5069e9e40f
Bob Nadler <bnadlerjr@gmail.com>
2014-12-27 00:03:08 -0500

    Run code metrics after deploy.

--- project.clj
+++ project.clj
@@ -30,4 +30,8 @@
              :test {
                     :env {:database-url "jdbc:postgresql://localhost/webdev_test"}}}
 
-  :plugins [[lein-environ "1.0.0"]])
+  :plugins [[lein-environ "1.0.0"]
+            [lein-ancient "0.5.5"]
+            [lein-kibit "0.0.8"]
+            [jonase/eastwood "0.2.1"]
+            [lein-bikeshed "0.2.0"]])
