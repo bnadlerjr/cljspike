3109228d6990ec0917d9d2758f85a69317f33029
Bob Nadler <bnadlerjr@gmail.com>
2014-12-27 13:13:47 -0500

    Update HTTP Kit and postgreSQL dependencies.

--- project.clj
+++ project.clj
@@ -9,10 +9,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.2"]
                  [compojure "1.3.1"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.19"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [java-jdbc/dsl "0.1.1"]
-                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
                  [hiccup "1.0.5"]
                  [environ "1.0.0"]]
 
