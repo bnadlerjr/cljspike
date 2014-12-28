f832d7fb9f5325781859241b8c78312b07a57131
Bob Nadler <bnadlerjr@gmail.com>
2014-12-27 23:18:38 -0500

    Refactor middleware tests.

--- test/webdev/common/middleware_test.clj
+++ test/webdev/common/middleware_test.clj
@@ -13,29 +13,26 @@
             response)))))
 
 (deftest wrap-simulated-methods-middleware
-  (testing "POST request with a DELETE param"
-    (let [middleware (wrap-simulated-methods identity)
-          request {:request-method :post
-                   :params {"_method" "DELETE"}}
-          response (middleware request)]
-      (is (= :delete (:request-method response)))))
+  (let [middleware (wrap-simulated-methods identity)]
+    (testing "POST request with a DELETE param"
+      (let [request {:request-method :post
+                     :params {"_method" "DELETE"}}
+            response (middleware request)]
+        (is (= :delete (:request-method response)))))
 
-  (testing "POST request with a PUT param"
-    (let [middleware (wrap-simulated-methods identity)
-          request {:request-method :post
-                   :params {"_method" "PUT"}}
-          response (middleware request)]
-      (is (= :put (:request-method response)))))
+    (testing "POST request with a PUT param"
+      (let [request {:request-method :post
+                     :params {"_method" "PUT"}}
+            response (middleware request)]
+        (is (= :put (:request-method response)))))
 
-  (testing "POST request without a _method"
-    (let [middleware (wrap-simulated-methods identity)
-          request {:request-method :post}
-          response (middleware request)]
-      (is (= :post (:request-method response)))))
+    (testing "POST request without a _method"
+      (let [request {:request-method :post}
+            response (middleware request)]
+        (is (= :post (:request-method response)))))
 
-  (testing "POST request with an unknown param"
-    (let [middleware (wrap-simulated-methods identity)
-          request {:request-method :post
-                   :params {"_method" "foo"}}
-          response (middleware request)]
-      (is (= :post (:request-method response))))))
+    (testing "POST request with an unknown param"
+      (let [request {:request-method :post
+                     :params {"_method" "foo"}}
+            response (middleware request)]
+        (is (= :post (:request-method response)))))))
